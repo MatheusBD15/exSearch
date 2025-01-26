e21fe37cfb61bde334deba2e82bf58f8c587d8bd
Matheus Dambrowski <matheusbd15@gmail.com>
2025-01-26 14:03:50 -0300

    start an empty task

--- lib/exSearch/page_consumer.ex
+++ lib/exSearch/page_consumer.ex
@@ -5,14 +5,16 @@ defmodule ExSearch.PageConsumer do
   @max_number_of_pages 10
 
   def start_link(url) do
-    Logger.info("PageConsumer received url #{inspect(url)}")
+    # Logger.info("PageConsumer received url #{inspect(url)}")
 
     if Page.get_total_pages() < @max_number_of_pages do
       Task.start_link(fn ->
         ExSearch.Crawler.work(url)
       end)
     else
-      {:ok, nil}
+      Task.start_link(fn ->
+        :ok
+      end)
     end
   end
 end
